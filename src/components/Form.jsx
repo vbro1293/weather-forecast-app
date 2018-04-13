da0c2346d06425715669c7d256d92971e7123b53
Vikki <vbro1293@gmail.com>
2018-04-13 19:29:07 +0100

    added onchange on input value to local state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,13 +3,42 @@ import React, { Component } from "react";
 import Input from "./Input";
 import Button from "./Button";
 
-class Form extends Component{
+class Form extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            input: "",
+        }
+
+        /* Bind methods to this */
+        this.submit = this.submit.bind(this);
+        this.change = this.change.bind(this);
+    }
+
+    submit(e){
+        // Prevent auto reload
+        e.preventDefault();
+
+        // Reset input field
+        this.setState({
+            input: ""
+        })
+    }
+
+    change(e){
+        // Update local state with current input value
+        this.setState({ 
+            input: e.target.value
+        })
+    }
+
     render(){
         return(
-            <div>
-                <Input />
+            <form onSubmit={ this.submit }>
+                <Input value={ this.state.value } onChange={ this.change }/>
                 <Button>Submit</Button>
-            </div>
+            </form>
         )
     }
 }
