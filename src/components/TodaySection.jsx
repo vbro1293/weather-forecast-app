090e2b2bd409a062f244450575039cacd334c029
Vikki <vbro1293@gmail.com>
2018-04-14 16:19:35 +0100

    add moments, displaying sunset

--- src/components/TodaySection.jsx
+++ src/components/TodaySection.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import TodayDetail from "./TodayDetail";
+import moment from "moment";
 
-const TodaySection = () => (
-    <section>
-        <TodayDetail>Current Temp</TodayDetail>
-        <TodayDetail>Current Weather</TodayDetail>
-        <TodayDetail>Sundown</TodayDetail>
-    </section>
-);
+const TodaySection = ({ weather }) => {
+    const weatherToday = weather.slice().toJS();
+    return (
+        <section>
+        { true  ?
+            <section>
+                {console.log(weatherToday)}
+                <h3>{weatherToday.title}</h3>
+                <TodayDetail  >Current Temp</TodayDetail>
+                <TodayDetail>Current Weather</TodayDetail>
+                <TodayDetail value={ moment(weatherToday.sun_set).fromNow() }>Sundown</TodayDetail>
+                {/* sun set - timeNow (dependent on timezone)*/}
+            </section>
+        :
+        <p>no</p>
+        }
+        </section>
+
+    )
+};
 
 export default TodaySection;
