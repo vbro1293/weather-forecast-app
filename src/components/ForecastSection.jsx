1aa41950d5ca0ebdf0e0857b534925aecf1ba643
Vikki <vbro1293@gmail.com>
2018-04-15 18:51:33 +0100

    error handle of invalid location input

--- src/components/ForecastSection.jsx
+++ src/components/ForecastSection.jsx
@@ -5,17 +5,17 @@ import WeekSection from "./WeekSection";
 const ForecastSection = ({ todayData, weekData, error }) => (
     <section>
         {/* No data- weekData returns Map with size 0, otherwise will return object literal */}
-        { weekData.size !== 0 ?
+        { !error ?
+            (weekData.size !== 0 ?
             <section>
                 <TodaySection todayData={ todayData } />
                 <WeekSection weekData={ weekData }>Six Day Forecast</WeekSection>
             </section>
+            : 
+            <p>Please enter a location</p>) 
         :
-            !error ?
-                <p>Please enter a location</p>
-            :
-                <p>Please try a different city</p>
-        }
+        <p>Please try a different city</p>
+    }
     </section>
 );
 
--- src/data/initial.js
+++ src/data/initial.js
@@ -2,7 +2,8 @@ import { Map } from "immutable";
 
 const initial = Map({
     today: Map({}),
-    week: Map({})
+    week: Map({}),
+    error: false,
 });
 
 export default initial;
