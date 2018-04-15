414f9ec6e613464dfeaa17b61de4002193ee0fc7
Vikki <vbro1293@gmail.com>
2018-04-15 11:25:43 +0100

    added moment-timezone, to set default timezone to entered location to support local time

--- src/components/TodaySection.jsx
+++ src/components/TodaySection.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import TodayDetail from "./TodayDetail";
-import moment from "moment";
+import moment from "moment-timezone";
+
 
 const TodaySection = ({ weather }) => {
     const weatherToday = weather.slice().toJS();
     const curTemp = weatherToday["consolidated_weather"]["5"]["the_temp"];
     const curWeather = weatherToday["consolidated_weather"]["5"]["weather_state_name"];
     const sundown = moment(weatherToday.sun_set).fromNow();
+    moment.tz.setDefault(weatherToday.timezone);
     return (
         <section>
-            {console.log(curWeather)}
-            <h3>{ weatherToday.title }</h3>
+            {console.log(weatherToday)}
+            <header>
+                <h1>{ weatherToday.title }</h1>
+                <h3>{moment(weatherToday.time).format('llll') }</h3>
+            </header>
             <TodayDetail value={ curTemp } >Current Temp</TodayDetail>
             <TodayDetail value={ curWeather }>Current Weather</TodayDetail>
             <TodayDetail value={ sundown }>Sundown</TodayDetail>
