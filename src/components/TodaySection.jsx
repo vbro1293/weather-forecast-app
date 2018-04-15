2992b364be4f88e3b9d525db966a44e6907199df
Vikki <vbro1293@gmail.com>
2018-04-15 12:02:44 +0100

    add highcharts library, addded some comments

--- src/components/TodaySection.jsx
+++ src/components/TodaySection.jsx
@@ -4,11 +4,15 @@ import moment from "moment-timezone";
 
 
 const TodaySection = ({ weather }) => {
+    //Copy of state, convert immutable to JS object
     const weatherToday = weather.slice().toJS();
+    //Set consts of values req'd
     const curTemp = weatherToday["consolidated_weather"]["0"]["the_temp"];
     const curWeather = weatherToday["consolidated_weather"]["0"]["weather_state_name"];
     const sundown = moment(weatherToday.sun_set).fromNow();
+    //set local timezone, to show correct time
     moment.tz.setDefault(weatherToday.timezone);
+   
     return (
         <section>
             {console.log(weatherToday)}
@@ -19,9 +23,8 @@ const TodaySection = ({ weather }) => {
             <TodayDetail value={ curTemp } >Current Temp</TodayDetail>
             <TodayDetail value={ curWeather }>Current Weather</TodayDetail>
             <TodayDetail value={ sundown }>Sundown</TodayDetail>
-            {/* sun set - timeNow (dependent on timezone)*/}
         </section>
-    )
+    );
 };
 
 export default TodaySection;
